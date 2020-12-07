74e0b51bafd16102b7ba4654b577d9d44f837d6f
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2020-12-07 12:44:01 -0500

    Implementing methods inside models.

--- routes/web.php
+++ routes/web.php
@@ -57,5 +57,66 @@ Route::get('/user', function () {
     //     'name' => 'Lilian Remon Reitor'
     // ]);
 
+    
+    // $user = \App\User::find(4);
+    //return $user->store; //returns a store object
+
+    //dd($user->store()); // returns an instance of hasOne relationship. 
+    //dd($user->store()->count()); //I can make a count to know how many stores the user has
+
+    // $store = \App\Store::find(1);
+    //return $store->products; //returns store's products
+    // return $store->products()->where('id', 9)->get(); // I can make a query with a condition
+
+    // $category = \App\Category::find(1);
+    // $category->products;
+
+
+    //Create store for an user
+    // $user = \App\User::find(10);
+    // $store = $user->store()->create([
+    //     'name' => 'Store test',
+    //     'description' => 'Store test for informatic products',
+    //     'mobile_phone' => 'XX-XXXXX-XXXX',
+    //     'phone' => 'XX-XXXXX-XXXX',
+    //     'slug' => 'loja-teste'
+    // ]);
+
+    //Create product for a store
+    // $store = \App\Store::find(41);
+    // $product = $store->products()->create([
+    //     'name' => 'Notebook Dell',
+    //     'description' => 'Core I5 10GB',
+    //     'body' => 'Qualquer coisa...',
+    //     'price' => 2999.90,
+    //     'slug' => 'notebook-dell',
+    // ]);
+
+    // dd($product);
+
+    //Creata Categories
+
+    // \App\Category::create([
+    //     'name' => 'Games',
+    //     'description' => null,
+    //     'slug' => 'games'
+    // ]);
+
+    // \App\Category::create([
+    //     'name' => 'Notebooks',
+    //     'description' => null,
+    //     'slug' => 'notebooks'
+    // ]);
+
+    //return \App\Category::all();
+
+    // $product = \App\Product::find(41);
+    // $product->categories()->attach([2]);// Associates Product to Category with id 2
+
+    // dd($product->categories()->detach([2]));// Dissociate Product to Category with id 2
+
+    // $product->categories()->sync([1,2]); // Syncronized the relationship, if doesn't exist creates it, if has other relation 
+                                        // in the database and it isn't passed as parameter to sync will be deleted
+
     return \App\User::all();
 });
