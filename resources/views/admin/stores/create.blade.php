354e400c1e35f1d953a2d48f06b31a6842ad4c43
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2020-12-08 09:24:27 -0500

    Creating routes, controller and view for Store.

---
+++ resources/views/admin/stores/create.blade.php
@@ -0,0 +1,36 @@
+<h1>Criar Loja</h1>
+
+<form action="/admin/stores/store" method="post">
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <div>
+        <label>Nome Loja</label>
+        <input type="text" name="name">
+    </div>
+    <div>
+        <label>Descrição</label>
+        <input type="text" name="description">
+    </div>
+    <div>
+        <label>Telefone</label>
+        <input type="text" name="phone">
+    </div>
+    <div>
+        <label>Celular/WhatsApp</label>
+        <input type="text" name="mobile_phone">
+    </div>
+    <div>
+        <label>Slug</label>
+        <input type="text" name="slug">
+    </div>
+    <div>
+        <label>Usuario</label>
+        <select name="user">
+            @foreach($users as $user)
+            <option value="{{$user->id}}">{{$user->name}}</option>
+            @endforeach
+        </select>
+    </div>
+    <div>
+        <button type="submit">Criar Loja</button>
+    </div>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     // return view('welcome', ['x' => $helloWorld]);
 });
 
+Route::get('/admin/stores', 'Admin\\StoreController@index');
+Route::get('/admin/stores/create', 'Admin\\StoreController@create');
+Route::post('/admin/stores/store', 'Admin\\StoreController@store');
+
 Route::get('/user', function () {
 //    $users = \App\User::all();
 
