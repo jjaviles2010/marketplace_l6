6bb9d965c8750d014a49d208ca9c857dbcd991f6
Jose Javier Aviles Gomez <jose.aviles.pro@gmail.com>
2020-12-21 09:20:03 -0500

    Implementing category controller,request and views

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::group(['middleware' => ['auth']], function(){
     
         Route::resource('stores', 'StoreController');
         Route::resource('products', 'ProductController');
+        Route::resource('categories', 'CategoryController');
     
     });
 
