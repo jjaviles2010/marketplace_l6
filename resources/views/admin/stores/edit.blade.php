5233a111d4dea8631a0454bcebfdc7c8b099ff7c
Jose Javier Aviles Gomez <jose.aviles.pro@gmail.com>
2021-01-05 09:29:23 -0500

    Including validation of images.

--- resources/views/admin/stores/edit.blade.php
+++ resources/views/admin/stores/edit.blade.php
@@ -29,7 +29,12 @@
             <img src="{{asset('storage/' . $store->logo)}}" alt="">
         </p>
         <label>Logo da Loja</label>
-        <input type="file" name="logo" class="form-control">
+        <input type="file" name="logo" class="form-control @error('logo') is-invalid @enderror">
+        @error('logo')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div class="form-group">
