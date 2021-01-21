8922f7ab96aaec32c32c62af2523f89b5e7035e3
Jose Javier Aviles Gomez <jose.aviles.pro@gmail.com>
2021-01-21 12:18:13 -0500

    Adjust in the store list.

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,25 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    @if(!$store)
-        <a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar Loja</a>
-    @endif
-
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Loja</th>
-                <th>Total de Produtos</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>{{$store->id}}</td>
-                <td>{{$store->name}}</td>
-                <td>{{$store->products->count()}}</td>
-                <td>
+@if(!$store)
+<a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar Loja</a>
+@else
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Loja</th>
+            <th>Total de Produtos</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>{{$store->id}}</td>
+            <td>{{$store->name}}</td>
+            <td>{{$store->products->count()}}</td>
+            <td>
                 <div class="btn-group">
                     <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-primary">EDITAR</a>
                     <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="POST">
@@ -28,8 +27,9 @@
                         <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
                     </form>
                 </div>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
+@endif
 @endsection
